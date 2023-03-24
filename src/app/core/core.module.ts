13e1095f9e8c1c204c4f2886d8b9e2bbd736935b
kniechniedowicz <kniechniedowic@objectivity.co.uk>
2023-03-24 14:27:20 +0100

    feat: add core module + add header component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  exports: [HeaderComponent],
+})
+export class CoreModule {}
