1a3facb95ac920733b88436beed14b4969073438
kniechniedowicz <kniechniedowic@objectivity.co.uk>
2023-03-29 13:05:31 +0200

    chore: remove movies list component

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MoviesRoutingModule } from './movies-routing.module';
-import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { MoviesService } from './services/movies.service';
 import { MatTableModule } from '@angular/material/table';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [MoviesComponent, MoviesListComponent, MovieComponent],
+  declarations: [MoviesComponent, MovieComponent],
   imports: [
     CommonModule,
     MoviesRoutingModule,
